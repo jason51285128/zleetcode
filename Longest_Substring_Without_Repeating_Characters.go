e471ab054a57560d7cf881c9e3ce682469801b4c
zhangchen <zhangchenwhu@qq.com>
2020-04-13 18:34:51 +0800

    Longest_Substring_Without_Repeating_Characters

---
+++ Longest_Substring_Without_Repeating_Characters.go
@@ -0,0 +1,58 @@
+package leetcode
+
+//最长不重复子序列
+
+//用一个map记录已出现的字符，变量保存当前最长，当出现重复字符时，起始位置设置为
+//前一个重复字符前面
+func lswrcGreedy(s string) int {
+	start := 0
+	lastpos := make(map[byte]int)
+	maxlength := 0
+	for i := 0; i < len(s); i++ {
+		index, ok := lastpos[byte(s[i])]
+		if ok {
+			if i-start > maxlength {
+				maxlength = i - start
+			}
+			start = index + 1
+		}
+		lastpos[byte(s[i])] = i
+	}
+	if len(s)-start > maxlength {
+		return len(s) - start
+	}
+	return maxlength
+
+}
+
+//l[i],以s[i]结尾的最长不重复子串的长度
+//l[i] = l[i - 1] + 1, if s[i] not repeat
+//l[i] = index{s[i]} - index{s[i] last repeat}
+func lswrcDP(s string) int {
+	if len(s) == 0 {
+		return 0
+	}
+	if len(s) == 1 {
+		return 1
+	}
+	maxlength := 1
+	curlen := 1
+	lastpos := make(map[byte]int)
+	lastpos[byte(s[0])] = 0
+	for i := 1; i < len(s); i++ {
+		index, ok := lastpos[byte(s[i])]
+		if !ok {
+			curlen++
+			if curlen > maxlength {
+				maxlength = curlen
+			}
+		} else {
+			curlen = i - index
+			if curlen > maxlength {
+				maxlength = curlen
+			}
+		}
+		lastpos[byte(s[i])] = i
+	}
+	return maxlength
+}
---
+++ Longest_Substring_Without_Repeating_Characters_test.go
@@ -0,0 +1,120 @@
+package leetcode
+
+import "testing"
+
+const (
+	inputcase1  = ""
+	expectcase1 = 0
+	inputcase2  = "x"
+	expectcase2 = 1
+	inputcase3  = "hello"
+	expectcase3 = 3
+	inputcase4  = "abcxdaefg"
+	expectcase4 = 8
+	inputcase5  = "abcdaefag"
+	expectcase5 = 6
+	inputcase6  = "abcdaefaglmnopq"
+	expectcase6 = 10
+	inputcase7  = "abcdefa"
+	expectcase7 = 6
+	inputcase8  = "abcdefgh"
+	expectcase8 = 8
+)
+
+//greedy method testcase
+func TestLswrcGreedy01(t *testing.T) {
+	if res := lswrcGreedy(inputcase1); res != expectcase1 {
+		t.Error("input: ", inputcase1, " expected: ", expectcase1, " got: ", res)
+	}
+}
+
+func TestLswrcGreedy02(t *testing.T) {
+	if res := lswrcGreedy(inputcase2); res != expectcase2 {
+		t.Error("input: ", inputcase2, " expected: ", expectcase2, " got: ", res)
+	}
+}
+
+func TestLswrcGreedy03(t *testing.T) {
+	if res := lswrcGreedy(inputcase3); res != expectcase3 {
+		t.Error("input: ", inputcase3, " expected: ", expectcase3, " got: ", res)
+	}
+}
+
+func TestLswrcGreedy04(t *testing.T) {
+	if res := lswrcGreedy(inputcase4); res != expectcase4 {
+		t.Error("input: ", inputcase4, " expected: ", expectcase4, " got: ", res)
+	}
+}
+
+func TestLswrcGreedy05(t *testing.T) {
+	if res := lswrcGreedy(inputcase5); res != expectcase5 {
+		t.Error("input: ", inputcase5, " expected: ", expectcase5, " got: ", res)
+	}
+}
+
+func TestLswrcGreedy06(t *testing.T) {
+	if res := lswrcGreedy(inputcase6); res != expectcase6 {
+		t.Error("input: ", inputcase6, " expected: ", expectcase6, " got: ", res)
+	}
+}
+
+func TestLswrcGreedy07(t *testing.T) {
+	if res := lswrcGreedy(inputcase7); res != expectcase7 {
+		t.Error("input: ", inputcase7, " expected: ", expectcase7, " got: ", res)
+	}
+}
+
+func TestLswrcGreedy08(t *testing.T) {
+	if res := lswrcGreedy(inputcase8); res != expectcase8 {
+		t.Error("input: ", inputcase8, " expected: ", expectcase8, " got: ", res)
+	}
+}
+
+//DP method testcase
+func TestLswrcDP01(t *testing.T) {
+	if res := lswrcDP(inputcase1); res != expectcase1 {
+		t.Error("input: ", inputcase1, " expected: ", expectcase1, " got: ", res)
+	}
+}
+
+func TestLswrcDP02(t *testing.T) {
+	if res := lswrcDP(inputcase2); res != expectcase2 {
+		t.Error("input: ", inputcase2, " expected: ", expectcase2, " got: ", res)
+	}
+}
+
+func TestLswrcDP03(t *testing.T) {
+	if res := lswrcDP(inputcase3); res != expectcase3 {
+		t.Error("input: ", inputcase3, " expected: ", expectcase3, " got: ", res)
+	}
+}
+
+func TestLswrcDP04(t *testing.T) {
+	if res := lswrcDP(inputcase4); res != expectcase4 {
+		t.Error("input: ", inputcase4, " expected: ", expectcase4, " got: ", res)
+	}
+}
+
+func TestLswrcDP05(t *testing.T) {
+	if res := lswrcDP(inputcase5); res != expectcase5 {
+		t.Error("input: ", inputcase5, " expected: ", expectcase5, " got: ", res)
+	}
+}
+
+func TestLswrcDP06(t *testing.T) {
+	if res := lswrcDP(inputcase6); res != expectcase6 {
+		t.Error("input: ", inputcase6, " expected: ", expectcase6, " got: ", res)
+	}
+}
+
+func TestLswrcDP07(t *testing.T) {
+	if res := lswrcDP(inputcase7); res != expectcase7 {
+		t.Error("input: ", inputcase7, " expected: ", expectcase7, " got: ", res)
+	}
+}
+
+func TestLswrcDP08(t *testing.T) {
+	if res := lswrcDP(inputcase8); res != expectcase8 {
+		t.Error("input: ", inputcase8, " expected: ", expectcase8, " got: ", res)
+	}
+}
